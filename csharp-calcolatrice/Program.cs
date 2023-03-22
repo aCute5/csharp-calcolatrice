1c822f4566bb52583e7298a9d43ec91ceb3f2206
Dario Costantini <costantini95dario@gmail.com>
2023-03-22 15:43:11 +0100

    Aggiungere i file di progetto.

---
+++ csharp-calcolatrice/Program.cs
@@ -0,0 +1,40 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System.ComponentModel;
+
+Console.WriteLine("Hello, World!");
+
+
+public class HelperCalc
+{
+    public static int Add(int a, int b)
+    {
+        return a + b;
+    }
+    public static double Add(double a,double b)
+    {
+        return a + b;
+    }
+    public static double Sottr(double a,double b)
+    {
+        return a - b;
+    }
+    public static int Sottr(int a,int b)
+    {
+        return a - b;
+    }
+    public static int Molt(int a,int b)
+    {
+        return a * b;
+    }
+    public static double Molt(int a,double b)
+    {
+        return a * b;
+    }
+    public static double Absolute(double a, double b)
+    {
+        if(a == 0)
+        {
+           a = 0
+        }
+    }
+}
